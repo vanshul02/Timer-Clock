effcc7dfe882423411c6e94baa46b0e8de19c41f
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2024-06-12 22:11:51 +0530

    Added basic structure and Add/Remove Timer functionality

---
+++ src/Components/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Timer = (props) => {
+    let id = props.id
+    console.log(id)
+    return (
+        <div>Timer</div>
+    )
+}
+
+export default Timer
---
+++ src/Pages/TimerHome.js
@@ -0,0 +1,37 @@
+import { React, useState } from 'react'
+import Timer from '../Components/Timer'
+
+
+const TimerHome = () => {
+    const [timerList, setTimerList] = useState([])
+
+    const removeTimer = (e) => {
+        let id = e.target.getAttribute('id')
+        console.log("Removed ID: ", id)
+        setTimerList(prevTimerList => prevTimerList.filter(timerObject => timerObject.id !== id))
+    }
+
+    const addTimer = () => {
+        let id = "timer" + String(timerList.length + 1)
+        let obj = {
+            id: id,
+            timer: <><Timer id = {id} /></>
+        }
+        setTimerList(prevTimerList => [...prevTimerList, obj])
+    }
+
+    return (
+        <>
+            <input type="button" className="add-timer-btn" onClick={addTimer} value="Add Timer" />
+            <div>{timerList.map(entry =>
+                <div key = {entry.id}>
+                    {entry.timer}
+                    <input id = {entry.id} type="button" className="remove-timer-btn" onClick={removeTimer} value="Remove Timer" />
+                </div>
+            )}
+            </div>
+        </>
+    )
+}
+
+export default TimerHome
