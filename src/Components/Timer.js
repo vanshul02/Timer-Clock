37581e40326392ad5893e5f9a4acd8e6dd03759f
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2024-06-12 23:01:09 +0530

    Added Logic for a timer

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,17 +1,56 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 
 
 const Timer = (props) => {
-    const [count, setCount] = useState(0)
-    let id = props.id
-    console.log(id)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+    const [isRunning, setIsRunning] = useState(null)
 
-    const addCountHandler = () => { setCount(oldCount => oldCount + 1) }
+    useEffect(() => {
+        let interval
+        if (isRunning) {
+            interval = setInterval(() => {
+                if (seconds > 0) {
+                    setSeconds(seconds => seconds - 1)
+                } else if (minutes > 0) {
+                    setSeconds(59)
+                    setMinutes(minutes => minutes - 1)
+                } else if (hours > 0) {
+                    setSeconds(59)
+                    setMinutes(59)
+                    setHours(hours => hours - 1)
+                }
+            }, 1000)
+        }
+        return () => { clearInterval(interval) }
+    }, [seconds, minutes, hours, isRunning])
+
+    const isRunningHandler = () => {
+        setIsRunning(isRunning => isRunning === null ? true : !isRunning)
+    }
+
+    const secondsHandler = (e) => {
+        let value = e.target.value
+        setSeconds(value)
+    }
+    const minutesHandler = (e) => {
+        let value = e.target.value
+        setMinutes(value)
+    }
+    const hoursHandler = (e) => {
+        let value = e.target.value
+        setHours(value)
+    }
 
     return (
         <>
-            <div>{count}</div>
-            <input type='button' onClick={addCountHandler} value="Add Count" />
+            <div className='timer-parent'>
+                <label>HH</label><input className='timer' value={hours} maxLength={2} onChange={hoursHandler} />
+                <label>MM</label><input className='timer' value={minutes} maxLength={2} onChange={minutesHandler} />
+                <label>SS</label><input className='timer' value={seconds} maxLength={2} onChange={secondsHandler} />
+                <input type="submit" value="Start" onClick={isRunningHandler} />
+            </div>
         </>
     )
 }
