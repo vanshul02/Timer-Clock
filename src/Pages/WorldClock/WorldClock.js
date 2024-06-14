e6e9ccea2eadc52a03211e1743e5317276ff3675
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2024-06-14 12:01:28 +0530

    Added API call logic to fetch all time zones

--- src/Pages/WorldClock/WorldClock.js
+++ src/Pages/WorldClock/WorldClock.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import axios from 'axios'
 
 const WorldClock = () => {
+  const [timeZoneList, setTimeZoneList] = useState([])
+
+  useEffect(() => {
+    axios.get("https://worldtimeapi.org/api/timezone/").then(
+      resp => {
+        console.log(resp.data)
+        let timeZones = resp.data
+        setTimeZoneList(timeZones)
+      }
+    )
+  }, [])
   return (
-    <div>WorldClock</div>
+    <>
+      <div className='time-zone-selector'>
+        <select name="time-zone" id="time-zone-select">
+          {
+            timeZoneList.map(timeZone =>
+              <option value={timeZone}>{timeZone}</option>
+            )
+          }
+        </select>
+      </div>
+    </>
   )
 }
 
