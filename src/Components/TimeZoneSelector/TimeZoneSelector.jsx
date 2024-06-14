ffcf6655c96e9cd1aaf1e723eb4e3ada36d33bf4
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2024-06-14 19:34:31 +0530

    Converted all js files to jsx as they aslo have HTML

--- src/Components/TimeZoneSelector/TimeZoneSelector.jsx
+++ src/Components/TimeZoneSelector/TimeZoneSelector.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react'
 import axios from 'axios'
-import ListSelector from '../../Components/ListSelector/ListSelector'
+import ListSelector from '../ListSelector/ListSelector'
 
 const TimeZoneSelector = (props) => {
     const [timeZoneList, setTimeZoneList] = useState([])
