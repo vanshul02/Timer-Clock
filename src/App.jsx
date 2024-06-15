85656a07c7f2defdcec0d64840783c473768dffc
Vanshul Ahuja <vanshul.ahuja@gmail.com>
2024-06-15 13:46:56 +0530

    Removed Home Page and Added Responsiveness CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home/Home';
 import TimerHome from './Pages/TimerHome/TimerHome';
 import WorldClock from './Pages/WorldClock/WorldClock';
 import NavBar from './Components/NavBar/NavBar';
@@ -10,7 +9,7 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Navigate to="/timer" />} />
         <Route path='/timer' element={<TimerHome />} />
         <Route path='/clock' element={<WorldClock />} />
       </Routes>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,7 +6,6 @@ const NavBar = () => {
     return (
         <>
             <ul className='navbar'>
-                <li><Link to="/"><button>Home</button></Link> </li>
                 <li><Link to="/timer"><button>Timer</button></Link></li>
                 <li><Link to="/clock"><button>World Clock</button></Link></li>
             </ul>
